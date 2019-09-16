642b68368dc44130464cb6843dfff83436e4f484
JavaScript Joe <jjprevite@gmail.com>
2019-09-15 19:03:37 -0700

    add slugify function

---
+++ src/slugify.js
@@ -0,0 +1,16 @@
+export function slugify(string) {
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .trimStart() // Trim - from start of text ES2019 feat
+    .trimEnd(); // Trim - from end of text - ES2019 feat
+  // .replace(/^-+/, '') // Trim - from start of text
+  // .replace(/-+$/, ''); // Trim - from end of text
+}
+
+// Source:
+// https://medium.com/@mhagemann/the-ultimate-way-to-slugify-a-url-string-in-javascript-b8e4a0d849e1
